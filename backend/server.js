00e9bbbdb50dfe2e16bd19604d0b732f0cd0f29f
AswalMohandas <aswalmohandas9633@gmail.com>
2026-06-24 09:32:02 +0530

    Fix Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const { connectDB} = require("./connect.js");
+const { connectDB } = require("./connect.js");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
@@ -9,14 +8,14 @@ const blogRoutes = require("./routes/blogRoutes");
 
 const app = express();
 
+// Connect to MongoDB
 connectDB();
-
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Static files (images)
+// Static files
 app.use("/images", express.static("public/images"));
 
 // Test route
@@ -24,17 +23,18 @@ app.get("/", (req, res) => {
   res.send("Server Working");
 });
 
-// Routes
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
 
+// Run locally only
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
 
-  
-// Start server
-const PORT = process.env.PORT
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
-
+// Export app for Vercel
 module.exports = app;
